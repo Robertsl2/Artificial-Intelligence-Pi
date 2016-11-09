4ab4b54fa25380888156a907a248e646017a9060
Robertsl2 <robertslouie1@gmail.com>
2016-11-08 23:29:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@
 #!/usr/bin/python
 import sys
 import time
-from sense_hat import SenseHat
+from grovepi import *
 import os
 import alsaaudio
 import wave
@@ -18,23 +18,23 @@ from evdev import InputDevice, list_devices, ecodes
 import alexa_helper # Import the web functions of Alexa, held in a separate program in this directory
 
 print "Welcome to Alexa. I will help you in anyway I can.\n  Press Ctrl-C to quit"
-
-sense = SenseHat() # Initialise the SenseHAT
-sense.clear()  # Blank the LED matrix
+'''
+#sense = SenseHat() # Initialise the SenseHAT
+#sense.clear()  # Blank the LED matrix
 
 # Search for the SenseHAT joystick
 found = False
 devices = [InputDevice(fn) for fn in list_devices()]
 for dev in devices:
     if dev.name == 'Raspberry Pi Sense HAT Joystick':
-        found = True 
+        found = True
         break
 
 # Exit if SenseHAT not found
 if not(found):
     print('Raspberry Pi Sense HAT Joystick not found. Aborting ...')
     sys.exit()
-
+'''
 # Initialise audio buffer
 audio = ""
 inp = None
@@ -44,7 +44,7 @@ colours = [[255, 0, 0], [255, 0, 0], [255, 105, 0], [255, 223, 0], [170, 255, 0]
 
 # Loudness for highest bar of RGB display
 max_loud = 1024
-
+'''
 # Given a "loudness" of speech, convert into RGB LED bars and display - equaliser style
 def set_display(loudness):
     mini = [[0,0,0]]*8
@@ -52,20 +52,20 @@ def set_display(loudness):
     mini[8-brightness:] = colours[8-brightness:]
     display = sum([[col]*8 for col in mini], [])
     sense.set_pixels(display)
-
+'''
 # When button is released, audio recording finishes and sent to Amazon's Alexa service
 def release_button():
     global audio, inp
-    sense.set_pixels([[0,0,0]]*64)
+    #sense.set_pixels([[0,0,0]]*64)
     w = wave.open(path+'recording.wav', 'w') # This and following lines saves voice to .wav file
     w.setnchannels(1)
     w.setsampwidth(2)
     w.setframerate(16000)
     w.writeframes(audio)
     w.close()
-    sense.show_letter("?") # Convert to question mark on display
+    #sense.show_letter("?") # Convert to question mark on display
     alexa_helper.alexa(sense) # Call upon alexa_helper program (in this directory)
-    sense.clear() # Clear display
+    #sense.clear() # Clear display
     inp = None
     audio = ""
 
@@ -94,7 +94,7 @@ def continue_pressed():
         audio += data
         a = numpy.fromstring(data, dtype='int16') # Converts audio data to a list of integers
         loudness = int(numpy.abs(a).mean()) # Loudness is mean of amplitude of sound wave - average "loudness"
-        set_display(loudness) # Set the display to show this "loudness"
+        #set_display(loudness) # Set the display to show this "loudness"
 
 # Event handler for button
 def handle_enter(pressed):
@@ -104,9 +104,12 @@ def handle_enter(pressed):
 # Continually loops for events, if event detected and is the middle joystick button, call upon event handler above
 def event_loop():
     try:
+        handle_enter(1)
+        '''
         for event in dev.read_loop(): # for each event
             if event.type == ecodes.EV_KEY and event.code == ecodes.KEY_ENTER: # if event is a key and is the enter key (middle joystick)
                 handle_enter(event.value) # handle event
+        '''
     except KeyboardInterrupt: # If Ctrl+C pressed, pass back to main body - which then finishes and alerts the user the program has ended
         pass
 
@@ -118,3 +121,4 @@ if __name__ == "__main__": # Run when program is called (won't run if you decide
     os.system('mpg123 -q {}hello.mp3'.format(path, path)) # Say hello!
     event_loop()
     print "\nYou have exited Alexa. I hope that I was useful. To talk to me again just type: python main.py"
+
